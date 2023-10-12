c4750203d9849968436ff958310d0dca4683821c
CedricNtwari <>
2023-10-12 09:12:05 +0000

    Clean up functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,25 +8,23 @@ displayTab(currentTab);
  * Display the specified tab of the form 
  * and update the navigation buttons.
  *
- * @param {number} n - The index of the tab to display.
+ * @param {number} i - The index of the tab to display.
  */
-function displayTab(n) {
-    // This function will display the specified tab of the form ...
-    var x = document.getElementsByClassName("tab");
-    x[n].style.display = "block";
-    // ... and fix the Previous/Next buttons:
-    if (n == 0) {
+function displayTab(i) {
+    let tab = document.getElementsByClassName("tab");
+    tab[i].style.display = "block";
+    // fix the Previous/Next buttons
+    if (i == 0) {
       document.getElementById("prevBtn").style.display = "none";
     } else {
       document.getElementById("prevBtn").style.display = "inline";
     }
-    if (n == (x.length - 1)) {
-      document.getElementById("nextBtn").innerHTML = "Submit";
+    if (i == (tab.length - 1)) {
+      document.getElementById("continueBtn").innerHTML = "Submit";
     } else {
-      document.getElementById("nextBtn").innerHTML = "Next";
+      document.getElementById("continueBtn").innerHTML = "Continue";
     }
   }
-
 
 
 /**
@@ -35,20 +33,25 @@ function displayTab(n) {
  * @param {number} n - A positive number to move to the next tab, or a negative number to move to the previous tab.
  * @returns {boolean} - Returns `false` if any field in the current tab is invalid and prevents navigation.
  */
-function nextPrev(n) {
-    // This function will figure out which tab to display
-    var x = document.getElementsByClassName("tab");
+function continueButton(n) {
+    let tab = document.getElementsByClassName("tab");
     
     // Hide the current tab:
-    x[currentTab].style.display = "none";
+    tab[currentTab].style.display = "none";
     
     // Increase or decrease the current tab by 1:
-    currentTab = currentTab + n;
+    currentTab += n;
     
-    // if you have reached the end of the form... :
-    if (currentTab >= x.length) {
-      //...the form gets submitted:
-      document.getElementById("regForm").submit();
+    // if you have reached the end of the form
+    if (currentTab >= tab.length) {
+      //the form gets submitted:
+      // Display an alert
+      alert('Thank you for your submission. We have received your request.');
+        
+      // Add a delay before reloading the page
+      setTimeout(function() {
+          window.location.reload();
+      }, 10);
       return false;
     }
     
