5387a8ad979c60703338ce4fb9dd16c8139fbf4e
CedricNtwari <>
2023-10-19 14:52:08 +0000

    Refactor code order

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,13 @@
 */
 /* jshint esversion: 6 */
                     
-                  "------- form tab navigation Code---------------"
+//                       --------- form tab navigation Code---------------
 
 // Current tab is set to be the first tab (0)
 var currentTab = 0;
 
 // Initialize the accordion functionality
-initAccordion();
+setupAccordion();
 
 // Display the current tab
 displayTab(currentTab);
@@ -114,13 +114,13 @@ document.getElementById("continueBtn").addEventListener("click", function () {
 });
 
 
-                            "------- Accordion Code ---------------"
+//          --------- Accordion Code ---------------
 
 /**
  * Function to initialize the accordion functionality.
  * Adds click event listeners to accordion elements.
  */
-function initAccordion() {
+function setupAccordion() {
     // Select all elements with the "accordion" class
     const accordions = document.getElementsByClassName('accordion');
 
@@ -144,7 +144,7 @@ function toggleAccordion(event) {
 }
 
 
-                        "------- Calculate receive amount Code ---------------"
+//             ------- Calculate receive amount Code ---------------
 
 
 // Define exchange rates for different countries
@@ -169,166 +169,178 @@ const exchangeRates = {
     },
 };
 
-// Function to update the "They receive" input based on exchange rate, selected country, and currency
-function updateReceiveAmount() {
-    const sendAmountInput = document.getElementById('send-amount');
-    const receiveAmountInput = document.getElementById('receive-amount');
-    const exchangeRateElement = document.getElementById('exchange-rate');
-    const countrySelect = document.getElementById('countrySelect');
-    const currencySelect = document.getElementById('currency-selected');
-    const currencyChoose = document.getElementById('currency-chosed');
-    const feePrice = document.getElementById('fee-price');
-    const price = document.getElementById('price');
-
-    function calculateReceiveAmount() {
-        const sendAmount = parseFloat(sendAmountInput.value);
-        const selectedCountry = countrySelect.value;
-        const selectedCurrency = currencySelect.value;
-
-        if (!isNaN(sendAmount) && selectedCountry in exchangeRates) {
-            const exchangeRate = exchangeRates[selectedCountry][selectedCurrency];
-            if (exchangeRate !== undefined) {
-                // Calculate the fee using the calculateFee function
-                calculateFee();
-                const feePriceElement = document.getElementById('fee-price');
-                const feeText = feePriceElement.textContent;
-
-                // Extract the fee value from the feeText
-                const feeMatch = feeText.match(/([+-]?\d+(\.\d+)?)\s+(\S+)/);
-
-                if (feeMatch && feeMatch.length === 4) {
-                    const feeValue = parseFloat(feeMatch[1]);
-                    const feeCurrency = feeMatch[3];
-
-                    // Add the fee to the sendAmount to get the total amount in the "You Send" currency
-                    const totalAmount = sendAmount + feeValue;
-
-                    // Update the fee and total amount in the "You Send" currency
-                    feePriceElement.textContent = `+ ${feeValue.toFixed(2)} ${feeCurrency}`;
-                    price.textContent = `${totalAmount.toFixed(2)} ${feeCurrency}`;
-
-                    // Update the displayed exchange rate with the selected currency symbol
-                    exchangeRateElement.textContent = `1.00 ${selectedCurrency} = ${exchangeRate} ${feeCurrency}`;
-                    receiveAmountInput.value = (sendAmount * exchangeRate).toFixed(2);
-                }
-            } else {
-                // Fallback to Euro if the selected currency is not found
-                currencySelect.value = 'EUR';
-                calculateReceiveAmount(); // Recalculate with the fallback currency
-            }
-        }
-    }
+const sendAmountInput = document.getElementById('send-amount');
+const receiveAmountInput = document.getElementById('receive-amount');
+const exchangeRateElement = document.getElementById('exchange-rate');
+const feePrice = document.getElementById('fee-price');
+const price = document.getElementById('price');
+const countrySelect = document.getElementById('countrySelect');
+const currencySelect = document.getElementById('currency-selected');
 
 
-    sendAmountInput.addEventListener('click', function () {
-        if (sendAmountInput.value === "0.00") {
-            sendAmountInput.value = ''; // Clear the input
-        }
-    });
-    sendAmountInput.addEventListener('input', function () {
-        if (!countrySelect.value) {
-            alert("Please choose a country before adding the 'You send' amount.");
-        }
-    });
-
-    sendAmountInput.addEventListener('input', calculateReceiveAmount);
-    currencySelect.addEventListener('change', calculateReceiveAmount);
-    // Add an event listener to the currency selection dropdown
-    currencyChoose.addEventListener('change', calculateReceiveAmount);
-
-    countrySelect.addEventListener('change', function () {
-        const selectedCountry = countrySelect.value;
-        if (selectedCountry in exchangeRates) {
-            const exchangeRate = exchangeRates[selectedCountry][currencySelect.value];
-            if (exchangeRate !== undefined) {
-                exchangeRateElement.textContent = `1.00 ${currencySelect.value} = ${exchangeRate} (${selectedCountry})`;
-            }
-
-            // Update the currency selection to match the selected country's available currencies
-            updateAvailableCurrencyOptions(selectedCountry);
-
-            // Trigger the calculation of receive amount
-            calculateReceiveAmount();
-        }
-    });
-
-    /**
-     * responsible for dynamically updating the available currency options
-     * in a select dropdown based on a selected country
-     * @param {*} selectedCountry 
-     */
-    function updateAvailableCurrencyOptions(selectedCountry) {
-        const sendAmount = parseFloat(sendAmountInput.value);
-        // To execute only when a country and a valid amount are selected. 
-        // Otherwise, potential errors are shown until the user provides the required input
-        if (selectedCountry && !isNaN(sendAmount)) {
-            // Check if exchangeRates for the selected country exist
-            if (exchangeRates[selectedCountry]) {
-                // Define the available currencies based on the selected country
-                const availableCurrencies = Object.keys(exchangeRates[selectedCountry]);
-                for (let i = 0; i < currencySelect.options.length; i++) {
-                    const option = currencySelect.options[i];
-                    if (availableCurrencies.includes(option.value)) {
-                        option.style.display = 'block';
-                    } else {
-                        option.style.display = 'none';
-                    }
-                }
-            } else {
-                // Handle the case where exchangeRates for the selected country do not exist
-                console.error(`Exchange rates for ${selectedCountry} are not defined.`);
-            }
-        }
-    }
-
-
-
-     /**
-      * Function to calculate fee based on 'You Send' amount and currency
-      * and update the fee price
-      */
-    function calculateFee() {
-        const sendAmount = parseFloat(sendAmountInput.value);
-        const selectedCurrency = currencySelect.value;
-        const currencyChosed = currencyChoose.value;
-
-
-        if (!isNaN(sendAmount)) {
-            let feePercentage;
-
-            if (selectedCurrency === 'EUR') {
-                feePercentage = 0.07; // 7% fee for EUR
-            } else if (selectedCurrency === 'USD') {
-                feePercentage = 0.06; // 6% fee for USD
-            } else if (selectedCurrency === 'BIF' || selectedCurrency === 'RWF') {
-                feePercentage = 0.05; // 5% fee for BIF and RWF
-            } else {
-                // Fallback to a default fee percentage if the currency is not recognized
-                feePercentage = 0.05;
-            }
-
-            // Calculate the fee
-            const fee = sendAmount * feePercentage;
-
-            // Update the fee price
-            feePrice.textContent = `+ ${fee.toFixed(2)} ${currencyChosed} (${(feePercentage * 100).toFixed(2)} %)`;
-        }
-    }
-
-    // Add an event listener to the 'send-amount' input
-    sendAmountInput.addEventListener('input', calculateFee);
-
-    // Add an event listener to the 'currency-selected' dropdown
-    currencySelect.addEventListener('change', calculateFee);
-
-    // Call the function to initialize the fee calculation
+// Add event listeners to elements
+sendAmountInput.addEventListener('input', handleSendAmountInput);
+sendAmountInput.addEventListener('input', function () {
     calculateFee();
+});
+sendAmountInput.addEventListener('focus', function () {
+    this.value = ''; // Clear the input
+});
+currencySelect.addEventListener('change', handleCurrencySelectChange);
+currencySelect.addEventListener('change', function () {
+    calculateFee();
+});
+document.getElementById('currency-chosed').addEventListener('change', handleCurrencyChosenChange);
+countrySelect.addEventListener('change', handleCountrySelectChange);
 
-    // Initialize the calculation and currency options
-    calculateReceiveAmount();
-    updateAvailableCurrencyOptions(countrySelect.value);
+
+function calculateReceiveAmount(sendAmount, selectedCountry, selectedCurrency) {
+    if (!isNaN(sendAmount) && selectedCountry in exchangeRates) {
+        const exchangeRate = exchangeRates[selectedCountry][selectedCurrency];
+        if (exchangeRate !== undefined) {
+            calculateFee(sendAmount, selectedCurrency);
+            renderResults(sendAmount, exchangeRate, selectedCurrency);
+        } else {
+            currencySelect.value = 'EUR';
+            calculateReceiveAmount(sendAmount, selectedCountry, 'EUR');
+        }
+    }
 }
 
-// Call the function to initialize the event listeners
-updateReceiveAmount();
+function renderResults(sendAmount, exchangeRate, selectedCurrency) {
+    // Your code for updating the display element
+    const fee = calculateFeeValue(sendAmount, selectedCurrency);
+    const feeCurrency = getFeeCurrency();
+    const totalAmount = sendAmount + fee;
+
+    feePrice.textContent = `+ ${fee.toFixed(2)} ${feeCurrency}`;
+    price.textContent = `${totalAmount.toFixed(2)} ${feeCurrency}`;
+    exchangeRateElement.textContent = `1.00 ${selectedCurrency} = ${exchangeRate} ${feeCurrency}`;
+    receiveAmountInput.value = (sendAmount * exchangeRate).toFixed(2);
+}
+
+
+// ---- Helper Functions --------
+
+
+function calculateFeeValue(sendAmount, selectedCurrency) {
+    const feePercentage = getFeePercentage(selectedCurrency);
+    return sendAmount * feePercentage;
+}
+
+function getFeePercentage(selectedCurrency) {
+    // Your code for getting fee percentage
+    if (selectedCurrency === 'EUR') {
+        return 0.07; // 7% fee for EUR
+    } else if (selectedCurrency === 'USD') {
+        return 0.06; // 6% fee for USD
+    } else if (selectedCurrency === 'BIF' || selectedCurrency === 'RWF') {
+        return 0.05; // 5% fee for BIF and RWF
+    } else {
+        // Fallback to a default fee percentage if the currency is not recognized
+        return 0.05;
+    }
+}
+
+
+function getFeeCurrency() {
+    const currencyChoose = document.getElementById('currency-chosed');
+    return currencyChoose.value;
+}
+
+
+function handleSendAmountInput() {
+    const sendAmount = parseFloat(this.value);
+    calculateReceiveAmount(sendAmount, countrySelect.value, currencySelect.value);
+    if (!countrySelect.value) {
+        alert("Please choose a country before adding the 'You send' amount.");
+    }
+}
+
+function handleCurrencySelectChange() {
+    const sendAmount = parseFloat(sendAmountInput.value);
+    const selectedCountry = countrySelect.value;
+    const selectedCurrency = this.value;
+    calculateReceiveAmount(sendAmount, selectedCountry, selectedCurrency);
+}
+
+function handleCurrencyChosenChange() {
+    const sendAmount = parseFloat(sendAmountInput.value);
+    const selectedCountry = countrySelect.value;
+    const selectedCurrency = currencySelect.value;
+    calculateReceiveAmount(sendAmount, selectedCountry, selectedCurrency);
+}
+
+function handleCountrySelectChange() {
+    const selectedCountry = this.value;
+    const selectedCurrency = currencySelect.value;
+
+    if (selectedCountry in exchangeRates) {
+        const exchangeRate = exchangeRates[selectedCountry][selectedCurrency];
+        if (exchangeRate !== undefined) {
+            const exchangeRateElement = document.getElementById('exchange-rate');
+            exchangeRateElement.textContent = `1.00 ${selectedCurrency} = ${exchangeRate} (${selectedCountry})`;
+        }
+
+        updateAvailableCurrencyOptions(selectedCountry);
+
+        const sendAmount = parseFloat(sendAmountInput.value);
+        calculateReceiveAmount(sendAmount, selectedCountry, selectedCurrency);
+    }
+}
+
+
+function calculateFee() {
+    const sendAmount = parseFloat(sendAmountInput.value);
+    const selectedCurrency = currencySelect.value;
+
+    if (!isNaN(sendAmount)) {
+        let feePercentage;
+
+        if (selectedCurrency === 'EUR') {
+            feePercentage = 0.07; // 7% fee for EUR
+        } else if (selectedCurrency === 'USD') {
+            feePercentage = 0.06; // 6% fee for USD
+        } else if (selectedCurrency === 'BIF' || selectedCurrency === 'RWF') {
+            feePercentage = 0.05; // 5% fee for BIF and RWF
+        } else {
+            // Fallback to a default fee percentage if the currency is not recognized
+            feePercentage = 0.05;
+        }
+
+        const fee = sendAmount * feePercentage;
+
+        // Update the fee price
+        feePrice.textContent = `+ ${fee.toFixed(2)} ${getFeeCurrency()} (${(feePercentage * 100).toFixed(2)} %)`;
+    }
+}
+
+function updateAvailableCurrencyOptions(selectedCountry, selectedCurrency) {
+    const sendAmount = parseFloat(sendAmountInput.value);
+
+    // To execute only when a country and a valid amount are selected.
+    // Otherwise, potential errors are shown until the user provides the required input
+    if (selectedCountry && !isNaN(sendAmount)) {
+        if (exchangeRates[selectedCountry]) {
+            const availableCurrencies = Object.keys(exchangeRates[selectedCountry]);
+            for (let i = 0; i < currencySelect.options.length; i++) {
+                const option = currencySelect.options[i];
+                if (availableCurrencies.includes(option.value)) {
+                    option.style.display = 'block';
+                } else {
+                    option.style.display = 'none';
+                }
+            }
+
+            // Set the selected currency to the first available currency
+            if (!availableCurrencies.includes(selectedCurrency)) {
+                currencySelect.value = availableCurrencies[0];
+                selectedCurrency = availableCurrencies[0];
+            }
+        } else {
+            console.error(`Exchange rates for ${selectedCountry} are not defined.`);
+        }
+    }
+    calculateReceiveAmount(sendAmount, selectedCountry, selectedCurrency);
+}
 
