4674a42a27d90307902ae67c4342019ae36f6bfe
CedricNtwari <>
2023-10-20 12:58:10 +0000

    Add utility module for ES6 support

--- assets/js/script.js
+++ assets/js/script.js
@@ -146,6 +146,8 @@ function toggleAccordion(event) {
 
 //             ------- Calculate receive amount Code ---------------
 
+import { getFeeCurrency, calculateFeeValue, handleCurrencySelectChange, handleCurrencyChosenChange } from './util.js'
+
 
 // Define exchange rates for different countries
 const exchangeRates = {
@@ -258,79 +260,6 @@ function renderResults(sendAmount, exchangeRate, selectedCurrency) {
 }
 
 
-// ---- Helper Functions --------
-
-/**
- * Get the selected fee currency from the 'currency-chosed' element
- * This helps to retrieve the fee currency dynamically
- * @param {*} sendAmount - Amount to be sent
- * @param {*} selectedCurrency - Selected currency code
- * @returns {number} - The calculated fee amount
- */
-function getFeeCurrency() {
-    const currencyChoose = document.getElementById('currency-chosed');
-    return currencyChoose.value;
-}
-
-
-/**
- * Calculate fee value based on  selected currency & send amount
- * @param {*} sendAmount - Amount to be sent
- * @param {*} selectedCurrency - Selected currency code
- * @returns {number} - Calculated fee amount
- */
-function calculateFeeValue(sendAmount, selectedCurrency) {
-    // Get the fee percentage based on the selected currency
-    const feePercentage = getFeePercentage(selectedCurrency);
-     // Calculate and return the fee amount
-    return sendAmount * feePercentage;
-}
-
-
-/**
- * Get fee percentage based on the selected currency
- * @param {*} selectedCurrency - Selected currency value (EUR, USD, RWF, BIF)
- * @returns {number} - Fee percentage for selected currency
- */
-function getFeePercentage(selectedCurrency) {
-    // Check the selected currency and return the corresponding fee percentage
-    if (selectedCurrency === 'EUR') {
-        return 0.07; // 7% fee for EUR
-    } else if (selectedCurrency === 'USD') {
-        return 0.06; // 6% fee for USD
-    } else if (selectedCurrency === 'BIF' || selectedCurrency === 'RWF') {
-        return 0.05; // 5% fee for BIF and RWF
-    } else {
-        // Fallback to a default fee percentage if the currency is not recognized
-        return 0.05;
-    }
-}
-
-
-/**
- * Handles change event when user selects different currency for receiving
- * Updates calculations based on selected currency
- */
-function handleCurrencySelectChange() {
-    const sendAmount = parseFloat(sendAmountInput.value);
-    const selectedCountry = countrySelect.value;
-    const selectedCurrency = this.value;
-    calculateReceiveAmount(sendAmount, selectedCountry, selectedCurrency);
-}
-
-
-/**
- * Handles change event when user selects different currency for sending
- * Updates calculations based on selected currency
- */
-function handleCurrencyChosenChange() {
-    const sendAmount = parseFloat(sendAmountInput.value);
-    const selectedCountry = countrySelect.value;
-    const selectedCurrency = currencySelect.value;
-    calculateReceiveAmount(sendAmount, selectedCountry, selectedCurrency);
-}
-
-
 /**
  * Handles change event when user selects a different country
  * Updates exchange rate information, available currency options & recalculates amounts
@@ -349,7 +278,7 @@ function handleCountrySelectChange() {
             // Update displayed exchange rate
             exchangeRateElement.textContent = `1.00 ${selectedCurrency} = ${exchangeRate} (${selectedCountry})`;
         }
-       
+
         // Update available currency options based on selected country
         updateAvailableCurrencyOptions(selectedCountry);
         // Get send amount as a numeric value
